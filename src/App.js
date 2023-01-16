c92020590a280445e057f051de4a2d5496224c13
shyali <shyali.narayan@gmail.com>
2023-01-16 18:06:55 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import{BrowserRouter as Router,Routes,Route}from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offer";
+import Profile from "./pages/Profile";
+import SignIn  from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/profile"element ={<Profile/>}/>
+        <Route path="/offer" element ={<Offers/>}/>
+        <Route path="/sign-in" element ={<SignIn/>}/>
+        <Route path="/sign-up" element ={<SignUp/>}/>
+        <Route path="/forgot-password" element ={<ForgotPassword/>}/>
+        {/* this the naming convention ,most of the people write like this  */}
+        {/* the sign-in is the path which we will be showing in the browser search bar  */}
+        {/* this is the way of creating pages through route  */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+// here we create react functional components 
+// react native snippits will help us to create react functional components by just writing "rfc"
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
