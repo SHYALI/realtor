d1fe6f67e5068fb211c4268e65c2c5f3d25c2a8c
shyali <shyali.narayan2002@gmail.com>
2023-06-07 22:58:43 +0530

    created sign-in sign-up forgot password pages and OAuth compound

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import {FcGoogle} from 'react-icons/fc'
+
+export default function OAuth() {
+  return (
+    <div>
+      <button className="flex item-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded ">
+        <FcGoogle className="text-2xl bg-white rounded-full"/>
+        continue with google
+      </button>
+    </div>
+  )
+}
